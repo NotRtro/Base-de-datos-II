d45a132c8c898f0341dfb9a3d57fc80fa24f5b16
Angel-Utec <91230666+Angel-Utec@users.noreply.github.com>
2023-04-09 19:00:09 -0500

    Terminado

--- Laboratorio1/flexible_lenght.cpp
+++ Laboratorio1/flexible_lenght.cpp
@@ -11,6 +11,15 @@ struct Alumno{
   string apellidos;
   string carrera;
   float mensualidad;
+
+  Alumno(){};
+
+  Alumno(const string &nombre, const string &apellidos, const string &carrera, float mensualidad) : nombre(nombre),
+                                                                                                      apellidos(
+                                                                                                              apellidos),
+                                                                                                      carrera(carrera),
+                                                                                                      mensualidad(
+                                                                                                              mensualidad) {};
 };
 
 struct VariableRecord{
@@ -55,7 +64,7 @@ struct VariableRecord{
         ifstream file(archivo.c_str(),ios::in|ios::binary|ios::ate);
         file.seekg(0,ios::beg);
         int cont = 0;
-        while(!file.eof() || cont <= pos){
+        while(!file.eof() && cont <= pos){
             if(cont < pos){
                 string temp = "";
                 getline(file,temp,'\n');
@@ -79,23 +88,80 @@ struct VariableRecord{
     };
 };
 
+void print_vector_alumno(vector<Alumno> alumno_temp){
+    for (auto i = alumno_temp.begin(); i != alumno_temp.end() ; i++) {
+        cout<<"Nombre: "<<i->nombre<<endl;
+        cout<<"Apellidos: "<<i->apellidos<<endl;
+        cout<<"Mensualidad: "<<i->mensualidad<<endl;
+        cout<<"Carrera: "<<i->carrera<<endl;
+        cout<<"-----------------------------------------"<<endl;
+    }
+}
+
+
+void test1(VariableRecord* variable){
+    vector<Alumno> alumno_temp = variable->load();
+    print_vector_alumno(alumno_temp);
+    cout<<"Agregando alumnos"<<endl;
+    cout<<"|||||||||||||||||||||||||||||||||||||||"<<endl;
+    Alumno* alumno_1 = new Alumno("Angel","Tito","CS",1590);
+    Alumno* alumno_2 = new Alumno("Ronaldo","Flores","CS",1427);
+    Alumno* alumno_3 = new Alumno("Rodo","Vilcarromero","CS",1412);
+    Alumno* alumno_4 = new Alumno("Pablo","Lachos","Civil",1320);
+    Alumno* alumno_5 = new Alumno("Sebas","Vasquez","Mecatronica",2000);
+    Alumno* alumno_6 = new Alumno("Mauricio","Espinoza","Electicidad",1999);
+    Alumno* alumno_7 = new Alumno("Nicolas","Nasi","Bioingenieria",1250);
+    Alumno* alumno_8 = new Alumno("Pedro","Ramirez","Administracion",1400);
+    Alumno* alumno_9 = new Alumno("Peter","Nilson","Civil",1692);
+    Alumno* alumno_10 = new Alumno("Roberto","Fernandez","Electrica",1945);
+    variable->add(*alumno_1);
+    variable->add(*alumno_2);
+    variable->add(*alumno_3);
+    variable->add(*alumno_4);
+    variable->add(*alumno_5);
+    variable->add(*alumno_6);
+    variable->add(*alumno_7);
+    variable->add(*alumno_8);
+    variable->add(*alumno_9);
+    variable->add(*alumno_10);
+    alumno_temp = variable->load();
+    print_vector_alumno(alumno_temp);
+}
+
+void test2(VariableRecord* variable){
+    vector<Alumno> alumno_temp = variable->load();
+    print_vector_alumno(alumno_temp);
+    cout<<"Dupliquemos los alumnos con ADD"<<endl;
+    cout<<"|||||||||||||||||||||||||||||||||||||||"<<endl;
+    Alumno alumno_0 = variable->readRecord(0);
+    Alumno alumno_1 = variable->readRecord(1);
+    Alumno alumno_2 = variable->readRecord(2);
+    Alumno alumno_3 = variable->readRecord(3);
+    Alumno alumno_4 = variable->readRecord(4);
+    Alumno alumno_5 = variable->readRecord(5);
+    Alumno alumno_6 = variable->readRecord(6);
+    Alumno alumno_7 = variable->readRecord(7);
+    Alumno alumno_8 = variable->readRecord(8);
+    Alumno alumno_9 = variable->readRecord(9);
+    Alumno alumno_10 = variable->readRecord(10);
+    variable->add(alumno_0);
+    variable->add(alumno_1);
+    variable->add(alumno_2);
+    variable->add(alumno_3);
+    variable->add(alumno_4);
+    variable->add(alumno_5);
+    variable->add(alumno_6);
+    variable->add(alumno_7);
+    variable->add(alumno_8);
+    variable->add(alumno_9);
+    variable->add(alumno_10);
+    alumno_temp = variable->load();
+    print_vector_alumno(alumno_temp);
+}
+
 int main(){
     VariableRecord* temp =  new VariableRecord;
-    vector<Alumno> prueba = temp->load();
-    Alumno* alumno_prueba = new Alumno;
-    *alumno_prueba = temp->readRecord(8);
-    /*string nombre, apellidos, carrera, mensualidad;
-    cout<<"Ingrese el nombre: "<<endl;
-    cin>>nombre;
-    cout<<"Ingrese el apellidos: "<<endl;
-    cin>>apellidos;
-    cout<<"Ingrese el carrera: "<<endl;
-    cin>>carrera;
-    cout<<"Ingrese el mensualidad: "<<endl;
-    cin>>mensualidad;
-    alumno_prueba->nombre = nombre;
-    alumno_prueba->apellidos = apellidos;
-    alumno_prueba->carrera = carrera;
-    alumno_prueba->mensualidad = stof(mensualidad);*/
-    //temp->add(*alumno_prueba);
+    VariableRecord* temp2 =  new VariableRecord;
+    test1(temp);
+    test2(temp2);
 };
